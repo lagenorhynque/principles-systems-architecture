cd48f14986258602305641d32ac4458c6b7d27ec
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-08-09 01:31:45 +0900

    fix indentation level

--- src/java/principles_systems_architecture/chapter2/section1/State.java
+++ src/java/principles_systems_architecture/chapter2/section1/State.java
@@ -1,10 +1,10 @@
 package principles_systems_architecture.chapter2.section1;
 
 enum State {
-  審査中,
-  承認済,
-  実施中,
-  終了,
-  差し戻し中,
-  中断中
+    審査中,
+    承認済,
+    実施中,
+    終了,
+    差し戻し中,
+    中断中
 }
--- src/java/principles_systems_architecture/chapter2/section1/StateTransitions.java
+++ src/java/principles_systems_architecture/chapter2/section1/StateTransitions.java
@@ -6,18 +6,18 @@ import java.util.Map;
 import java.util.Set;
 
 class StateTransitions {
-  Map<State, Set<State>> allowed;
+    Map<State, Set<State>> allowed;
 
-  {
-    allowed = new HashMap<>();
-    allowed.put(State.審査中, EnumSet.of(State.承認済, State.差し戻し中));
-    allowed.put(State.差し戻し中, EnumSet.of(State.審査中, State.終了));
-    allowed.put(State.承認済, EnumSet.of(State.実施中, State.終了));
-    allowed.put(State.実施中, EnumSet.of(State.中断中, State.終了));
-    allowed.put(State.中断中, EnumSet.of(State.実施中, State.終了));
-  }
+    {
+        allowed = new HashMap<>();
+        allowed.put(State.審査中, EnumSet.of(State.承認済, State.差し戻し中));
+        allowed.put(State.差し戻し中, EnumSet.of(State.審査中, State.終了));
+        allowed.put(State.承認済, EnumSet.of(State.実施中, State.終了));
+        allowed.put(State.実施中, EnumSet.of(State.中断中, State.終了));
+        allowed.put(State.中断中, EnumSet.of(State.実施中, State.終了));
+    }
 
-  boolean canTransit(State from, State to) {
-    return allowed.get(from).contains(to);
-  }
+    boolean canTransit(State from, State to) {
+        return allowed.get(from).contains(to);
+    }
 }
