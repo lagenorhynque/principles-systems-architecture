6eb9a3a620c867713d9cc4d7779af40cbf0adbc1
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2022-04-27 01:03:39 +0900

    Update dependencies

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![CircleCI](https://circleci.com/gh/lagenorhynque/principles-systems-architecture.svg?style=svg)](https://circleci.com/gh/lagenorhynque/principles-systems-architecture)
 
-Study notes on [『現場で役立つシステム設計の原則』](http://gihyo.jp/book/2017/978-4-7741-9087-7)(*Principles of the Systems Architecture*).
+Study notes on [『現場で役立つシステム設計の原則』](https://gihyo.jp/book/2017/978-4-7741-9087-7)(*Principles of the Systems Architecture*).
--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [prismatic/schema "1.1.6"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [prismatic/schema "1.2.0"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :profiles {:dev {:source-paths ["dev/clj"]
-                   :plugins [[jonase/eastwood "0.2.4"]
-                             [lein-cljfmt "0.5.6"]
-                             [lein-kibit "0.1.5"]]
+                   :plugins [[jonase/eastwood "1.2.3"]
+                             [lein-cljfmt "0.8.0"]
+                             [lein-kibit "0.1.8"]]
                    :aliases {"lint" ^{:doc "Execute cljfmt check, eastwood and kibit."}
                              ["do" ["cljfmt" "check"] ["eastwood"] ["kibit"]]}}})
